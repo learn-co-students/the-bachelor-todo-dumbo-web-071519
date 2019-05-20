70767d61c3ba20b443b6f9605004599aaf43a296
Ashna Shah-Grover <as19@williams.edu>
2019-05-20 04:16:29 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,59 @@
+require 'pry' 
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data[season].each do |contestant_info|
+    contestant_info.each do |attribute, value|
+      if value == "Winner"
+        return contestant_info["name"].split[0]
+      end 
+    end 
+  end 
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |season, season_info|
+    season_info.each do |contestant_info|
+      contestant_info.each do |attribute, value|
+        if value == occupation
+          return contestant_info["name"]
+        end 
+      end 
+    end 
+  end   
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  i = 0
+  data.each do |season, season_info|
+    season_info.each do |contestant_info|
+      contestant_info.each do |attribute, value|
+        if value == hometown
+          i += 1
+        end 
+      end 
+    end 
+  end 
+  return i
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |season, season_info|
+    season_info.each do |contestant_info|
+      contestant_info.each do |attribute, value|
+        if value == hometown 
+          return contestant_info["occupation"]
+        end 
+      end
+    end 
+  end 
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  ages = []
+  data[season].each do |contestant_info|
+    ages.push(contestant_info["age"].to_f)
+  end 
+  sum = 0
+  ages.each { |a| sum+=a }
+  average = (sum / ages.size).round
 end
