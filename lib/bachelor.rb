314b640290e128dc3de9c0c931e8b0918d5cb57f
vertreesa <Alessandra.Vertrees82@myhunter.cuny.edu>
2019-07-16 09:35:40 -0400

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -36,7 +36,7 @@ def count_contestants_by_hometown(data, hometown)
   data.each do |season, contestants|
     num += contestants.count{|contestant| contestant["hometown"]==hometown}
   end
-  hash = contestants(data)
+  #hash = contestants(data)
   return num
 
 end
